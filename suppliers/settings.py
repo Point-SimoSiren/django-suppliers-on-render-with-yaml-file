66acdce0f4f214393ebefb15ebf591193e4ba3b6
Pont-SimoSiren <sirensimo7@gmail.com>
2024-08-23 01:04:23 +0300

    debug false

--- suppliers/settings.py
+++ suppliers/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tmwl((@nmfes@jnuu$zl6fwq$bgg!pk6i_faqmqhlbd7e5dbug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
